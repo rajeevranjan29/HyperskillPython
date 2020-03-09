f280d424a11132e716fbe57bc54c1733c989b244
Peter Sims <ragtimesims@gmail.com>
2020-03-08 21:13:29 -0400

    rps

--- Rock-Paper-Scissors/Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/Rock-Paper-Scissors/task/rps/game.py
@@ -1,22 +1,25 @@
 # Write your code here
 import random
-guess = input()
 comp_options = ['rock', 'paper', 'scissors']
-comp_guess = random.choice(comp_options)
-
-if guess == comp_guess:
-    print(f'There is a draw ({comp_guess})')
-elif guess == 'rock' and comp_guess == 'paper':
-    print(f'Sorry, but computer chose {comp_guess}')
-elif guess == 'rock' and comp_guess == 'scissors':
-    print(f'Well done. Computer chose {comp_guess} and failed')
-elif guess == 'paper' and comp_guess == 'scissors':
-    print(f'Sorry, but computer chose {comp_guess}')
-elif guess == 'paper' and comp_guess == 'rock':
-    print(f'Well done. Computer chose {comp_guess} and failed')
-elif guess == 'scissors' and comp_guess == 'rock':
-    print(f'Sorry, but computer chose {comp_guess}')
-elif guess == 'scissors' and comp_guess == 'paper':
-    print(f'Well done. Computer chose {comp_guess} and failed')
 
 
+while(True):
+    comp_guess = random.choice(comp_options)
+    guess = input()
+    if guess == comp_guess:
+        print(f'There is a draw ({comp_guess})')
+    elif guess == 'rock' and comp_guess == 'paper':
+        print(f'Sorry, but computer chose {comp_guess}')
+    elif guess == 'rock' and comp_guess == 'scissors':
+        print(f'Well done. Computer chose {comp_guess} and failed')
+    elif guess == 'paper' and comp_guess == 'scissors':
+        print(f'Sorry, but computer chose {comp_guess}')
+    elif guess == 'paper' and comp_guess == 'rock':
+        print(f'Well done. Computer chose {comp_guess} and failed')
+    elif guess == 'scissors' and comp_guess == 'rock':
+        print(f'Sorry, but computer chose {comp_guess}')
+    elif guess == 'scissors' and comp_guess == 'paper':
+        print(f'Well done. Computer chose {comp_guess} and failed')
+    elif guess == '!exit':
+        print('Bye!')
+        break
